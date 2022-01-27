b17dbce769b0cbb55f2cbe9e0be598b03e11e85c
Guillaume CAMUZEAUX <gucamuze@e2r1p8.clusters.42paris.fr>
2022-01-27 17:52:24 +0100

    test script update

---
+++ update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+vogsphere_folder="~/cursus42/push_swap";
+git_folder="~/cursus42/repos_git/42cursus_push_swap";
+
+if [ !$1 ]
+then
+    echo "script needs an argument for commit message";
+else
+    cd ../../push_swap
+    pwd
+    git add .
+    git commit -m "$1"
+    git push
+    cd ../repos_git/42cursus_push_swap
+    git add .
+    git commit -m "$1"
+    git push
+fi
